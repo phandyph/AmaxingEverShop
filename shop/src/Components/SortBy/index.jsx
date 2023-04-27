9eb965701c766a58a4d2c2be804238e0ff13c941
phandyph <phornphandy23@gmail.com>
2023-04-27 15:37:36 +0700

    change some style

--- shop/src/Components/SortBy/index.jsx
+++ shop/src/Components/SortBy/index.jsx
@@ -2,22 +2,22 @@ import "./SortBy.scss";
 import SelectIcon from "../../Assets/Icons/SelectIcon";
 const SortBy = ({ sortList }) => {
   return (
-    <div className="sortAction">
-      <div className="sortContainer">
-        <span className="sortBy">Sort By: </span>
-        <div className="select">
-          <select name="" id="">
-            {sortList.map((item) => {
-              return (
-                <option key={item.id} value={item.value}>
-                  {item.label}
-                </option>
-              );
-            })}
-          </select>
-          <div className="selectIcon">
-            <SelectIcon />
-          </div>
+    <div className="sortContainer">
+      <div className="sort">
+        <span>Sort By: </span>
+      </div>
+      <div className="select">
+        <select name="" id="">
+          {sortList.map((item) => {
+            return (
+              <option key={item.id} value={item.value}>
+                {item.label}
+              </option>
+            );
+          })}
+        </select>
+        <div className="selectIcon">
+          <SelectIcon />
         </div>
       </div>
     </div>
