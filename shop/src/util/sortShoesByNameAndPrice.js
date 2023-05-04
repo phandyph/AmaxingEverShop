5d1c1345a98267dc0ec49fb33c6b685a91cd0884
phandyph <phornphandy23@gmail.com>
2023-05-04 09:55:56 +0700

    change some code

--- shop/src/util/sortShoesByNameAndPrice.js
+++ shop/src/util/sortShoesByNameAndPrice.js
@@ -1,14 +1,12 @@
 export const sortShoesByNameAndPrice = ({ optionChosen, disItemAfterChosen }) => {
     switch (optionChosen) {
       case "retail_price_cents":
-          disItemAfterChosen = disItemAfterChosen.sort((a, b) => (a.retail_price_cents > b.retail_price_cents ? 1 : -1));
-          break;
+          return disItemAfterChosen.sort((a, b) => (a.retail_price_cents > b.retail_price_cents ? 1 : -1));
       
       case "name":
-        disItemAfterChosen = disItemAfterChosen.sort((a, b) =>
+        return disItemAfterChosen.sort((a, b) =>
         a.name > b.name ? 1 : -1
-        );
-        break;        
+        );      
 
       default :
         return disItemAfterChosen;
